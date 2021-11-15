4b5484cc6e67126db7bd346eb93d7f075a9296a1
steven43533 <steven43533@gmail.com>
2021-11-15 15:47:40 -0500

    Commit before changes to DB

---
+++ controllers/watchList.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// index route to show the users want to watch list 
+router.get('/', (req,res) =>{
+    db.wantToWatch.findAll({
+        where: { userId: res.locals.currentUser.id }
+    })
+        .then(watchThese => {
+            console.log(watchThese);
+            res.render('indexWantToWatch', {results: watchThese})
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
+
+// posts selected anime to want to watch list
+router.post('/addToWatchList', (req,res) => {
+    const data = JSON.parse(JSON.stringify(req.body))
+    console.log('this is anime data', data);
+    db.wantToWatch.create({
+        name: data.animeTitle,
+        animeId: data.animeId,
+        userId: res.locals.currentUser.id,
+        posterImage: data.poster,
+        synopsis: data.synopsis
+    })
+    .then(createdFave => {
+        res.redirect(`/addToWatchList/`)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(created => {
+        console.log(created);
+    })
+})
+
+
+
+module.exports = router
